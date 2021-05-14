9aa1721a6c8283935bd69f8ce98bf6119b29bc40
martin enrique <martinenrique.pe@gmail.com>
2021-05-14 09:57:43 -0500

    primer commit mundo sadhguru

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Casper',
-    description: 'A port of the casper blog built for gatsby',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'Mundo Sadhguru',
+    description: 'Un blog para expresar las palabras de un gran yogi.',
+    siteUrl: 'https://mundosadhguru.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
@@ -112,7 +112,7 @@ module.exports = {
               }
             `,
             output: '/rss.xml',
-            title: 'Ghost\'s Blog',
+            title: 'Mundo Sadhguru\'s Blog',
             match: '^/blog/',
           },
         ],
--- src/website-config.ts
+++ src/website-config.ts
@@ -49,20 +49,20 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Mundo Sadhguru',
+  description: 'Un blog que expresa la filosofia de un gran yogi del siglo 21',
   coverImage: 'img/blog-cover.png',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  siteUrl: 'https://mundosadhguru.com',
+  facebook: 'https://www.facebook.com/mundosadhguru',
+  twitter: 'https://twitter.com/mundosadhguru',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
-  footer: 'is based on Gatsby Casper',
+  footer: 'desea un planeta lleno de amor, luz y risas.',
 };
 
 export default config;
